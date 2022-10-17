9ae83c0da7f22140175a3bf159691d4dd9b33fd2
Dmitrii Belikhin <70821842+belikhin@users.noreply.github.com>
2022-10-17 23:07:36 +0300

    Added new solutions

---
+++ src/JavaSyntax/level02/task01/Solution.java
@@ -0,0 +1,14 @@
+package JavaSyntax.level02.task01;
+
+/*
+Баги робокопа
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        int id = 1254;
+        int age = 27;
+        String documents = "passport";
+        String behavior = "ok";
+    }
+}
