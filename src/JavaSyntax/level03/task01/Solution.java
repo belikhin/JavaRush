e20ac1fc311fad886f4ef68181eb681e861c509f
Dmitrii Belikhin <70821842+belikhin@users.noreply.github.com>
2022-10-20 23:42:14 +0300

    Added a new solution (JavaSyntax.level03.task01)

---
+++ src/JavaSyntax/level03/task01/Solution.java
@@ -0,0 +1,16 @@
+package JavaSyntax.level03.task01;
+
+/*
+Ворота фабрики
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        boolean isFabricOpen = true;
+        if (isFabricOpen) {
+            System.out.println("Дверь открыта");
+        } else {
+            System.out.println("Дверь закрыта");
+        }
+    }
+}
