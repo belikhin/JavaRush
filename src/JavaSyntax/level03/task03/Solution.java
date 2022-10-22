8890700b9f6daf2bb2715875b5b951c83436f729
Dmitrii Belikhin <70821842+belikhin@users.noreply.github.com>
2022-10-22 03:05:26 +0300

    Added a new solution (JavaSyntax.level03.task03-05)

---
+++ src/JavaSyntax/level03/task03/Solution.java
@@ -0,0 +1,20 @@
+package JavaSyntax.level03.task03;
+
+import java.util.Scanner;
+
+/*
+Призывная кампания
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        String militaryCommissar = ", явитесь в военкомат";
+        Scanner scan = new Scanner(System.in);
+        String name = scan.nextLine();
+        int age = scan.nextInt();
+
+        if (age >= 18 && age <= 28) {
+            System.out.println(name + militaryCommissar);
+        }
+    }
+}
